696fe1c093c016e20434238fe5a5882edd1d934a
Ariel COHEN <arcohen@c3r2s5.wethinkcode.co.za>
2018-12-06 11:34:26 +0200

    done

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arcohen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 13:37:55 by arcohen           #+#    #+#             */
+/*   Updated: 2018/06/15 19:08:31 by arcohen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+# define BUFF_SIZE 1024
+
+int		get_next_line(const int fd, char **line);
+
+#endif
